7f961306b7281eca409baebb765c61e18e005eb2
Farrien <g-alb-ert@yandex.ru>
2018-08-30 18:15:46 +0500

    0.5.8c
    Deleted unnecessary public and vc files

--- public/selection.php
+++
@@ -1,15 +0,0 @@
-<?
-# Prevent access from direct calls
-defined('SN_Start') or header('HTTP/1.1 404 Not Found');
-
-//	//	28.08.2018//	Author: The Big D//	?>
-
-<button onclick="cave()">PRESS</button>
-
-<script>
-function cave(){
-	mr.Query('/apartments/', {request_type : 1, param_sections : [1,2,3], param_cost_range : [1000000, 4000000], param_area_range : {0:20, 1:55}, param_floors_range : [1, 2], param_rooms_range : [1, 3]}, function(r) {
-		console.log(r);
-	});
-}
-</script>
